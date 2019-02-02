cabe527f992402db4c1bfcb8be5ffed973236317
KrylovBoris <hakkanx@gmail.com>
2019-02-02 14:14:03 +0300

    1) Added character prototype
    2) Created class that translates user input to actions in game
    3) Added basic Strike
    4) Added swapping between states

---
+++ Assets/PrototypeFolderbyChorm/Scripts/Character.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    Vector3 startingPos;
+    Quaternion startingRot;
+    Animator anim;
+    public bool isBlocking;
+    bool isSprinting;
+    public enum CharacterState {SheathedSword, SwordStance, MagicStance};
+    public GameObject SwordParticles, MaskParticles;
+    private CharacterState currentState = CharacterState.SheathedSword;
+
+
+    public bool IsSprinting
+    {
+        get
+        {
+            return isSprinting;
+        }
+        set 
+        {
+            isSprinting = value;
+            anim.SetBool("isSprinting", value);
+        }
+    }
+
+    public CharacterState Status
+    {
+        get
+        {
+            return currentState;
+        }
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+        startingPos = transform.position;
+        startingRot = transform.rotation;
+        currentState = CharacterState.SheathedSword;
+        SwordParticles.SetActive(false);
+        MaskParticles.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+
+    public void SwapPlayerStatus()
+    {
+       switch (currentState)
+       {
+            case CharacterState.SheathedSword:
+                {
+                    DrawSword();
+                    //currentState = CharacterState.MagicStance;
+                    break;
+                }
+            case CharacterState.MagicStance:
+                {
+                    MaskParticles.SetActive(false);
+                    SwordParticles.SetActive(true);
+                    currentState = CharacterState.SwordStance;
+                    break;
+                }
+            case CharacterState.SwordStance:
+                {
+                    SwordParticles.SetActive(false);
+                    MaskParticles.SetActive(true);
+                    currentState = CharacterState.MagicStance;
+                    break;
+                }
+        }
+
+
+    }
+
+    public void DrawSword()
+    {
+        if (currentState == CharacterState.SheathedSword)
+        {
+            SwordParticles.SetActive(true);
+            MaskParticles.SetActive(false);
+            anim.SetTrigger("UnsheatheSword");
+            currentState = CharacterState.SwordStance;
+        }
+        else
+        {
+            if (currentState == CharacterState.SwordStance || currentState == CharacterState.MagicStance)
+            {
+                SwordParticles.SetActive(false);
+                MaskParticles.SetActive(false);
+                anim.SetTrigger("SheatheSword");
+                currentState = CharacterState.SheathedSword;
+            }
+        }
+    }
+
+    public void SwingSword()
+    {
+        anim.SetTrigger("Swing");
+    }
+
+}
---
+++ Assets/PrototypeFolderbyChorm/Scripts/Control.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour {
+
+    //public variables
+    public float speed = 10.0f;
+    public Vector3 drag = new Vector3(1, 1, 1);
+    public Quaternion previousRotation;
+    public float mass = 10.0f;
+    public float sensitivity = 60.0f;
+    public float CameraFollowRadius = 5.0f;
+    public float sprintModifier = 1.25f;
+    public float dashModifier = 5.4f;
+    public float dashTime = 0.2f;
+    //public float radius = 5.0f;
+    float dashStart;
+
+    // private variables
+    Ray groundedRay;
+    Vector3 Direction = Vector3.zero;
+    Vector3 CamDirectionForward, CamDirectionRight;
+    Vector3 targetPosition;
+    CharacterController charControl;
+    Character character;
+    public Transform PlayerCharacter;
+    Vector2 originalMousePosition;
+    float horizontalAngle, verticalAngle;
+    bool isDashing = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        character = gameObject.GetComponent<Character>();
+        targetPosition = PlayerCharacter.position;
+        charControl = gameObject.GetComponent<CharacterController>();
+        originalMousePosition = Input.mousePosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isDashing = Time.time < dashStart + dashTime;
+        CamDirectionForward = new Vector3(Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z);
+        CamDirectionRight = new Vector3(Camera.main.transform.right.x, 0, Camera.main.transform.right.z);
+        if (!isDashing)
+        {
+            Direction = Vector3.zero;
+            groundedRay = new Ray(transform.position, Vector3.down);
+            Direction += CamDirectionRight * Input.GetAxis("Horizontal") * speed;
+            Direction += CamDirectionForward * Input.GetAxis("Vertical") * speed;
+            Direction += (!charControl.isGrounded) ? new Vector3(0, Physics.gravity.y * mass, 0) : Vector3.zero;
+
+            if (Input.GetButtonDown("Attack"))
+            {
+                if(character.Status == Character.CharacterState.SwordStance)
+                {
+                    character.SwingSword();
+                }
+            }
+
+            if (Input.GetButtonDown("SwitchState"))
+            {            
+                character.SwapPlayerStatus();
+            }
+
+            //if (Input.GetButton("Block"))
+            //{
+            //    character.isBlocking = true;
+            //}
+            //else
+            //{
+            //    character.isBlocking = false;
+            //}
+
+            if (Input.GetButtonDown("Unsheathe"))
+            {
+                character.DrawSword();
+            }
+
+        }
+
+        horizontalAngle = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        verticalAngle = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        if(Input.GetButtonDown("Sprint"))
+        {
+            switch (character.Status)
+            {
+                case Character.CharacterState.MagicStance:
+                case Character.CharacterState.SwordStance:
+                case Character.CharacterState.SheathedSword:
+                    {
+                        character.IsSprinting = true;
+                        break;
+                    }
+            }
+            
+        }
+
+        if (Input.GetButtonUp("Sprint"))
+        {
+            switch (character.Status)
+            {
+                case Character.CharacterState.MagicStance:
+                case Character.CharacterState.SwordStance:
+                case Character.CharacterState.SheathedSword:
+                    {
+                        character.IsSprinting = false;
+                        break;
+                    }
+            }
+
+        }
+
+        if (character.IsSprinting)
+        {
+            Direction *= sprintModifier;
+        }
+
+        if(isDashing)
+        {
+            Direction *= dashModifier;
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(0, 0, 100, 100), ((int)(1.0f / Time.smoothDeltaTime)).ToString());
+    }
+
+    private void LateUpdate()
+    {
+        if (Direction != Vector3.zero)
+        {
+            charControl.Move(Direction * Time.deltaTime);
+            transform.forward = -CamDirectionRight;
+            previousRotation = transform.rotation;
+        }
+        else
+        {
+            transform.rotation = previousRotation;
+        }
+        Camera.main.transform.position = Camera.main.transform.position + (PlayerCharacter.position - targetPosition);
+        if (horizontalAngle != 0 || verticalAngle != 0)
+        {
+            Camera.main.transform.RotateAround(PlayerCharacter.position, PlayerCharacter.up, horizontalAngle);
+            Camera.main.transform.RotateAround(PlayerCharacter.position, CamDirectionRight, -verticalAngle);
+            Camera.main.transform.LookAt(PlayerCharacter, PlayerCharacter.up);
+        }
+
+        targetPosition = PlayerCharacter.position;
+    }
+}
