86611f12b5dc5566b9ca7c05ccb547adf86ca59c
Max Bukhalov <buchalovmaxim@gmail.com>
2019-02-23 23:43:00 +0300

    Created ISaveable interface, SaveObject and SaveLoadController. TODO: implement Save and Load functions for each type of GameObject, that return(or load from) SaveObject

---
+++ Assets/Scripts/Save Scripts/ISaveable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface ISaveable
+{
+    SaveObject Save();
+    void Load(SaveObject saveObject);
+}
---
+++ Assets/Scripts/Save Scripts/SaveLoadController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class SaveLoadController : MonoBehaviour
+{
+    public string fileExtension = ".save";
+    public string dataPath;
+
+    void Start()
+    {
+        dataPath = Path.Combine(Application.persistentDataPath, gameObject.scene.name + fileExtension);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.O) == true)
+        {
+            SaveProgress(dataPath);
+        }
+    }
+
+    void SaveProgress(string path)
+    {
+        SaveObject globalSaveObject = new SaveObject(gameObject.transform);
+        foreach (GameObject gameObject in FindObjectsOfType<GameObject>())
+        {
+            foreach(ISaveable saveable in gameObject.GetComponentsInChildren<ISaveable>())
+            {
+                SaveObject saveObject = saveable.Save();
+                globalSaveObject.fieldsToSave[gameObject.name] = saveObject;
+            }
+        }
+        SerializeSaveObject(globalSaveObject, path);
+    }
+
+    void LoadProgress(string path)
+    {
+        SaveObject globalSaveObject = DeserializeSaveObject(path);
+        foreach (GameObject gameObject in FindObjectsOfType<GameObject>())
+        {
+            foreach (ISaveable saveable in gameObject.GetComponentsInChildren<ISaveable>())
+            {
+                SaveObject saveObject = (SaveObject)globalSaveObject.fieldsToSave[gameObject.name];
+                saveable.Load(saveObject);
+            }
+        }
+    }
+
+    static void SerializeSaveObject(SaveObject saveObject, string path)
+    {
+        string jsonString = JsonUtility.ToJson(saveObject);
+
+        using (StreamWriter streamWriter = File.CreateText(path))
+        {
+            streamWriter.Write(jsonString);
+        }
+    }
+
+    static SaveObject DeserializeSaveObject(string path)
+    {
+        using (StreamReader streamReader = File.OpenText(path))
+        {
+            string jsonString = streamReader.ReadToEnd();
+            return JsonUtility.FromJson<SaveObject>(jsonString);
+        }
+    }
+}
