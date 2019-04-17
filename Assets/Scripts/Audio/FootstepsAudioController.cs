1001ce5793540cf5c99e7c0a9a242feb780e6f62
KraynovAlexander <kraynovsasha@gmail.com>
2019-04-17 14:09:12 +0300

    added music and sound objects
    Shitty footsteps, ambient in scene 1
    objects for various sounds

---
+++ Assets/Scripts/Audio/FootstepsAudioController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepsAudioController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if((transform.parent.parent.GetComponent<MovementSystem>().Moving)&&(!GetComponent<AudioSource>().enabled))
+        {
+            GetComponent<AudioSource>().enabled = true;
+        }
+        if((!transform.parent.parent.GetComponent<MovementSystem>().Moving)&& (GetComponent<AudioSource>().enabled))
+        {
+            GetComponent<AudioSource>().enabled = false;
+        }
+    }
+}
