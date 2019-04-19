efaa63fe40810504bce194e7fef8ae2dd692e259
KrylovBoris <hakkanx@gmail.com>
2019-04-19 07:54:35 +0300

    shoot mechanics

--- Assets/Scripts/Behaviours/PlayerShootBehaviour.cs
+++ Assets/Scripts/Behaviours/PlayerShootBehaviour.cs
@@ -2,17 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnarmedIdleBehaviour : StateMachineBehaviour
+public class PlayerShootBehaviour : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        var sheathSystem = animator.GetComponent<SheathSystem>();
-        if (sheathSystem && sheathSystem.State == SheathSystem.SheathSystemState.Sheathing)
-        {
-            sheathSystem.FinalizeSheathing();
-        }
-    }
+    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
